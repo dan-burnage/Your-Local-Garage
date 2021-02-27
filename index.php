e49665e0eb080e6998ff07de7fdce615825c1d4a
Daniel Burnage <50490246+dan-burnage@users.noreply.github.com>
2021-02-27 15:18:06 +0000

    Page structure updates and superuser template implimentation

--- index.php
+++ index.php
@@ -7,8 +7,13 @@
 	<cms:editable name='title' label='Title' type='text' group='landing_hero' order='2' />
 	<cms:editable name='button' label='Button text' type='text' group='landing_hero' order='3' />
 
-
 </cms:template>
+
+<cms:pages masterpage='settings.php' >
+
+	<cms:set g_show_grid=show_grid 'global' />
+
+</cms:pages>
 
 <!DOCTYPE html>
 
@@ -58,8 +63,14 @@
 
 	<div class="wrapper footer">
 
-		<div class="col-3 caption">Footer text</div>
-		<div class="align-right"><img class="portfolio-link" src="/static/images/smile-logo.svg" /></div>
+		<div class="caption align-left">
+			<span class="caption">Copyright © <cms:date format='Y' />  — Your Local Garage Ltd &nbsp;&nbsp;|&nbsp;&nbsp;</span>
+			<img class="footer-link social-icon" src="/static/images/facebook.svg" />
+			<img class="footer-link social-icon" src="/static/images/instagram.svg"/>
+			<img class="footer-link social-icon" src="/static/images/twitter.svg"/>
+		</div>
+
+		<div class="align-right"><a href="https://www.danburnage.com" target="_blank"><img class="footer-link" src="/static/images/smile-logo.svg" /></a></div>
 
 	</div>
 
